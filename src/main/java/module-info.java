31d7ec1660f4ae943900c47ce2fe72106072d460
Ling4826 <143516833+Ling4826@users.noreply.github.com>
2025-11-03 10:12:22 +0700

    100 percent complete

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,14 +4,10 @@ module se233.contrabossfight {
     requires javafx.media;
     requires java.logging;
 
-    // ✅ เปิดให้ JavaFX เข้าถึง MainApplication
     opens se233.contrabossfight to javafx.graphics, javafx.fxml;
-
-    // ✅ ส่งออก package หลักและอื่น ๆ ที่ใช้ในเกม
     exports se233.contrabossfight;
     exports se233.contrabossfight.game;
     exports se233.contrabossfight.character;
 
-    // ✅ สำหรับ FXML (เช่น Controller)
     opens se233.contrabossfight.game to javafx.fxml;
 }
